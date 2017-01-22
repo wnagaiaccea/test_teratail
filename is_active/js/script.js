cc2bfeb169b1aec227f2870486a18a15d03f8379
WATARU <wataru775@gmail.com>
2017-01-22 21:10:59 +0900

    hoge

--- is_active/js/script.js
+++ is_active/js/script.js
@@ -5,7 +5,6 @@ var activeUrls = location.pathname.split("/");
     navList = $(".side__navigation").find("a");
     isFllag = false;
     navList.each(parse);
-
 function parse(){
   var hrefs = $(this).attr("href").split("/");
   if(hrefs[hrefs.length - 1].length == 0){
@@ -18,27 +17,14 @@ function parse(){
 
 var isFllag = false;
 function isActive(urlElementsA,urlElementsB){
-  console.log("target A : " + urlElementsA);
-  console.log("target B : " + urlElementsB);
-  console.log( " target A : " + urlElementsA[urlElementsA.length - 1]);
-  console.log( " target B : " + urlElementsB[urlElementsB.length - 1]);
-  console.log(urlElementsA[urlElementsA.length - 1] === "");
-  console.log(urlElementsB[urlElementsB.length -1] === "");
-  console.log(urlElementsA[urlElementsA.length - 1] === "" && urlElementsB[urlElementsB.length -1] === "");
-  console.log( "*********");
   if(urlElementsA[urlElementsA.length - 1] === urlElementsB[urlElementsB.length -1]){
     return true;
   }
-  // if(urlElementsB[urlElementsB.length - 1].endsWith("html")){
-  //   urlElementsB.pop();
-  //   return isActive(urlElementsA,urlElementsB);
-  // }
   if(!isFllag)return false;
   if(urlElementsA[urlElementsA.length - 1].endsWith("html")){
     urlElementsA.pop();
     isFllag = true;
     return isActive(urlElementsA,urlElementsB);
   }
-
     return false;
 }
