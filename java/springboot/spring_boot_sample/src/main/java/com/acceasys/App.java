b3fda1ba3f31be46fa2ea2de8408c43361c71069
Wataru Nagai <w_nagai@acceasys.com>
2017-01-27 16:05:10 +0900

    add 63540 SPRINT BOOTga

---
+++ java/springboot/spring_boot_sample/src/main/java/com/acceasys/App.java
@@ -0,0 +1,13 @@
+package com.acceasys;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
