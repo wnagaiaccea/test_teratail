7474a651244253d7e39a1c38aae537b423bb3e94
WATARU <wataru775@gmail.com>
2017-02-10 00:17:09 +0900

    ImageMagickのお遊び追加2

---
+++ imagemagick/font_arc_3.sh
@@ -0,0 +1,7 @@
+convert -size 800x100 xc:lightblue -font fonts/ipaexg.ttf -pointsize 72 \
+-stroke black -strokewidth 14 -annotate +25+65 'あくせあシステム開発' \
+-stroke white -strokewidth 10 -annotate +25+65 'あくせあシステム開発' \
+-stroke black -strokewidth  5 -annotate +25+65 'あくせあシステム開発' \
+-fill orange -strokewidth  2 -annotate +25+65 'あくせあシステム開発' \
+-distort Arc 120  -trim +repage \
+  font_arc3.jpg
